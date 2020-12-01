97669d6f46e909bed28f352c614e14be778cdc64
Michael Malinak <m.malinak@gmail.com>
2020-11-30 22:44:32 -0500

    Created stubs for Day 1

---
+++ Advent2020/Day01/Day01a.swift
@@ -0,0 +1,8 @@
+//
+//  Day01a.swift
+//  Advent2020
+//
+//  Created by Michael Malinak on 11/30/20.
+//
+
+import Foundation
---
+++ Advent2020/Day01/Day01b.swift
@@ -0,0 +1,8 @@
+//
+//  Day01b.swift
+//  Advent2020
+//
+//  Created by Michael Malinak on 11/30/20.
+//
+
+import Foundation
