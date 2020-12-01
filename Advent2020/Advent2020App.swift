6c64615f56a9a1148c251c1ba32561cce5c5f2ef
Michael Malinak <m.malinak@gmail.com>
2020-11-30 22:30:15 -0500

    Initial Commit

---
+++ Advent2020/Advent2020App.swift
@@ -0,0 +1,17 @@
+//
+//  Advent2020App.swift
+//  Advent2020
+//
+//  Created by Michael Malinak on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Advent2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Advent2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Advent2020
+//
+//  Created by Michael Malinak on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
