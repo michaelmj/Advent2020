bfe42cea5d38ed8194b1ea283e9717b6e077568f
Michael Malinak <m.malinak@gmail.com>
2020-12-04 22:03:11 -0500

    Day 4 done.. this looks awful

--- Advent2020/Day04/Day04.swift
+++ Advent2020/Day04/Day04.swift
@@ -7,12 +7,113 @@
 
 import Foundation
 
+extension Array {
+
+   /// Unzip an `Array` of key/value tuples.
+   ///
+   /// - Returns: A tuple with two arrays, an `Array` of keys and an `Array` of values.
+
+   func unzip<K, V>() -> ([K], [V]) where Element == (key: K, value: V) {
+      var keys = [K]()
+      var values = [V]()
+
+      keys.reserveCapacity(count)
+      values.reserveCapacity(count)
+
+      forEach { key, value in
+         keys.append(key)
+         values.append(value)
+      }
+
+      return (keys, values)
+   }
+}
+
 struct Day04 {
+
+   let requiredCodes = Set(["byr",
+                            "iyr",
+                            "eyr",
+                            "hgt",
+                            "hcl",
+                            "ecl",
+                            "pid"])
+
+   let eyeColors = Set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+
    static func partA(_ data: String ) -> Int {
-      return 0
+      let today = Day04()
+      let parsed = today.parseData(dataString: data)
+
+      print(parsed)
+
+      let correct = parsed.filter {
+         Set($0.keys).isSuperset(of: today.requiredCodes)
+      }
+
+      print(correct)
+
+
+      return correct.count
+   }
+
+   static func vhgt(_ height: String) -> Bool {
+      if let numPrefix = Int(height.prefix(while: { "0"..."9" ~= $0 })) {
+         if ( height.contains("cm") ) {
+            return numPrefix >= 150 && numPrefix <= 193
+         } else if ( height.contains("in") ) {
+            return numPrefix >= 59 && numPrefix <= 76
+         }
+      }
+      return false
+   }
+
+   static func vhcl(_ hexColor: String) -> Bool {
+      return (hexColor.count == 7) &&
+         (hexColor.range(of: "#([a-f0-9]{6})", options: .regularExpression, range: nil, locale: nil) != nil)
+   }
+
+   static func vpid(_ passId: String) -> Bool {
+      return (passId.count == 9) &&
+         (passId.range(of: "([0-9]{9})", options: .regularExpression, range: nil, locale: nil) != nil)
    }
 
    static func partB(_ data: String ) -> Int {
-      return 0
+      let today = Day04()
+      let parsed = today.parseData(dataString: data)
+
+      print(parsed)
+
+      let correct = parsed.filter {
+         ( Set($0.keys).isSuperset(of: today.requiredCodes) &&
+            Int($0["byr"]!)! >= 1920 && Int($0["byr"]!)! <= 2002 &&
+            Int($0["iyr"]!)! >= 2010 && Int($0["iyr"]!)! <= 2020 &&
+            Int($0["eyr"]!)! >= 2020 && Int($0["eyr"]!)! <= 2030 &&
+            vhgt($0["hgt"]!) && vhcl($0["hcl"]!) && today.eyeColors.contains($0["ecl"]!) &&
+            vpid($0["pid"]!)
+         )
+      }
+
+      print(correct)
+
+
+      return correct.count
+   }
+
+
+
+   func parseData( dataString: String) -> [[String:String]] {
+
+      let lines = dataString.components(separatedBy: "\n\n").compactMap {
+         $0.components(separatedBy: .whitespacesAndNewlines).sorted().compactMap {
+            $0.components(separatedBy: ":")
+         }
+      }.compactMap {
+         $0.reduce(into: [String: String]()) {
+            $0[$1[0]] = $1[1]
+         }
+      }
+
+      return lines
    }
 }
--- Advent2020Tests/Day04Tests.swift
+++ Advent2020Tests/Day04Tests.swift
@@ -10,27 +10,28 @@ import XCTest
 class Day04Tests: XCTestCase {
 
    let testData = """
-..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
 """
 
 
    func testDay04a() throws {
-      XCTAssertEqual( Day04.partA( testData ), 7 )
-    //  print ( Day04.partA(Day04Data.string) )
+      XCTAssertEqual( Day04.partA( testData ), 2 )
+      print ( Day04.partA(Day04Data.string) )
    }
-//
-//   func testDay04b() throws {
-//      XCTAssertEqual( Day04.partB( testData ), 1 )
-//      print ( Day03.partB( Day04Data.string ) )
-//   }
+
+   func testDay04b() throws {
+      print ( Day04.partB( Day04Data.string ) )
+   }
 }
